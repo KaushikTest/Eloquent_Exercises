fd0e70c38b934942d36d2d2295fb58c245b1235d
KaushikTest <kaushik6990@gmail.com>
2025-07-23 22:13:10 +0530

    first program
    first JS program to print a # triangle

---
+++ triangle.js
@@ -0,0 +1,29 @@
+function make_triangle() {
+
+    for (let i = 0; i < 7; i++) {
+        switch (i) {
+            case 0:
+                console.log('#');
+                break;
+            case 1:
+                console.log('##');
+                break;
+            case 2:
+                console.log('###');
+                break;
+            case 3:
+                console.log('####');
+                break;
+            case 4:
+                console.log('#####');
+                break;
+            case 5:
+                console.log('######');
+                break;
+            default:
+                console.log('#######');
+                break;
+        }
+    }
+}
+make_triangle();
